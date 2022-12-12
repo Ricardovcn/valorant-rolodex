bb3eef452a74d535fd80c5dca464ae0c912ea6ce
Ricardovcn <ricardovitorcn@gmail.com>
2022-12-12 10:19:34 -0300

    adds a seach input to filter agents

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component{
 
     this.state = {
       agents: [],
+      search_key: '',
     };
   }
 
@@ -14,17 +15,25 @@ class App extends Component{
     fetch('https://valorant-api.com/v1/agents?isPlayableCharacter=true')
       .then((response) => response.json())
       .then((json_response) => this.setState(() => {
-        return { agents: json_response['data'] }
+        return { agents: json_response['data'], filtered_agents: json_response['data']}
       }, () => {
         console.log(this.state)
       }))
   }
 
   render () {
+    const filtered_agents = this.state.agents.filter((agent) => {
+      return agent.displayName.toLocaleLowerCase().includes(this.state.search_key)
+    })
+
     return (
       <div className="App">
+        <input placeholder='Search Agent' onChange={ (event) => {
+          this.setState(() => { return { search_key: event.target.value.toLocaleLowerCase() }})
+        }}></input>
+
         {
-          this.state.agents.map((agent) => {
+          filtered_agents.map((agent) => {
             return <h1 key={agent.uuid}>{agent.displayName}</h1>
           })
         }
