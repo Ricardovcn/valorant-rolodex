cd3a6937f8d68fb3bbf2aacc13a1bf8ee1510874
Ricardovcn <ricardovitorcn@gmail.com>
2022-12-12 08:43:14 -0300

    adds a simple map to show agent names

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      agents: [
+        {
+          id: '1',
+          name: 'Fade'
+        },
+        {
+          id: '2',
+          name: 'Sova'
+        },
+        {
+          id: '3',
+          name: 'Sage'
+        },
+        {
+          id: '4',
+          name: 'Jett'
+        },
+      ]
+    };
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {
+          this.state.agents.map((agent) => {
+            return <h1 key={agent.id}>{agent.name}</h1>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
